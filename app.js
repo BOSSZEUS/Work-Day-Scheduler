232947180499d487fb4f1535a3f1b02087de6774
jordan <jordanscheftic@yahoo.com>
2020-01-31 18:35:18 -0800

    some logic

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ let hour =['9am','10am', '11am','12am', '1pm', '2pm', '3pm', '4pm', '5pm','6pm' 
 let set = json.parse(localStorage.getItem('table')) || []
 set.length = hour.length
 
-let time = moment()
-let 
+var day = moment.unix(1318781876);
+let time = moment('01/31/2020', 'mm/dd/yy').format('x')
+console.log(time)
